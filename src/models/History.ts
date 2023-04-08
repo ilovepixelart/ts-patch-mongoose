1f7c473692f7419d945b34aa10fb6ce4b9f4780a
Alex Eagle <theherocraft@gmail.com>
2023-04-08 14:33:59 +0200

    Model

---
+++ src/models/History.ts
@@ -0,0 +1,39 @@
+import { Schema, model } from 'mongoose'
+
+import type IHistory from '../interfaces/IHistory'
+
+const HistorySchema = new Schema<IHistory>({
+  op: {
+    type: String,
+    required: true
+  },
+  modelName: {
+    type: String,
+    required: true
+  },
+  collectionName: {
+    type: String,
+    required: true
+  },
+  collectionId: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  doc: {
+    type: Object
+  },
+  patch: {
+    type: Array
+  },
+  version: {
+    type: Number,
+    min: 0,
+    default: 0
+  }
+}, { timestamps: true })
+
+HistorySchema.index({ op: 1, modelName: 1, collectionName: 1, collectionId: 1, version: 1 })
+
+const History = model('History', HistorySchema, 'history')
+
+export default History
