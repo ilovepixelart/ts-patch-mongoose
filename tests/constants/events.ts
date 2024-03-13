a259ab6057bba7f6830135c02adb6ec7b62d5a99
Reed Krantz <programming@krantz.one>
2024-03-13 11:55:33 -0500

    added plugin - global array tests

--- tests/constants/events.ts
+++ tests/constants/events.ts
@@ -1,3 +1,7 @@
 export const USER_CREATED = 'user-created'
 export const USER_UPDATED = 'user-updated'
 export const USER_DELETED = 'user-deleted'
+
+export const GLOBAL_CREATED = 'global-created'
+export const GLOBAL_UPDATED = 'global-updated'
+export const GLOBAL_DELETED = 'global-deleted'
