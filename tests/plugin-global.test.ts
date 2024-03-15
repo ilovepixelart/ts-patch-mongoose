6f05d6fc823c59bcc98f63a5d18f313fa30bec25
Reed Krantz <programming@krantz.one>
2024-03-15 11:55:24 -0500

    added objectID tests

--- tests/plugin-global.test.ts
+++ tests/plugin-global.test.ts
@@ -334,4 +334,205 @@ describe('plugin - global', () => {
       patch: third.patch,
     })
   })
+
+  it('should save objectID', async () => {
+    const john = await User.create({ name: 'John', role: 'user' })
+    expect(john.name).toBe('John')
+    const alice = await User.create({ name: 'Alice', role: 'user' })
+    expect(alice.name).toBe('Alice')
+    const product = await Product.create({ name: 'paper', addedBy: john })
+    expect(product.name).toBe('paper')
+
+    product.addedBy = alice._id
+    await product.save()
+
+    const history = await History.find({})
+    expect(history).toHaveLength(4)
+
+    const [first, second, third, fourth] = history
+
+    // 1 create
+    expect(first.version).toBe(0)
+    expect(first.op).toBe('create')
+    expect(first.modelName).toBe('User')
+    expect(first.collectionName).toBe('users')
+    expect(first.collectionId).toEqual(john._id)
+
+    expect(first.doc).toHaveProperty('_id', john._id)
+    expect(first.doc).toHaveProperty('name', 'John')
+    expect(first.doc).toHaveProperty('role', 'user')
+    expect(first.doc).toHaveProperty('createdAt')
+    expect(first.doc).toHaveProperty('updatedAt')
+
+    expect(first.patch).toHaveLength(0)
+
+    // 2 create
+
+    expect(second.version).toBe(0)
+    expect(second.op).toBe('create')
+    expect(second.modelName).toBe('User')
+    expect(second.collectionName).toBe('users')
+    expect(second.collectionId).toEqual(alice._id)
+
+    expect(second.doc).toHaveProperty('_id', alice._id)
+    expect(second.doc).toHaveProperty('name', 'Alice')
+    expect(second.doc).toHaveProperty('role', 'user')
+    expect(second.doc).toHaveProperty('createdAt')
+    expect(second.doc).toHaveProperty('updatedAt')
+
+    expect(second.patch).toHaveLength(0)
+
+    // 3 create
+    expect(third.version).toBe(0)
+    expect(third.op).toBe('create')
+    expect(third.modelName).toBe('Product')
+    expect(third.collectionName).toBe('products')
+    expect(third.collectionId).toEqual(product._id)
+
+    expect(third.doc).toHaveProperty('_id', product._id)
+    expect(third.doc).toHaveProperty('name', 'paper')
+    expect(third.doc).toHaveProperty('addedBy', john._id)
+    expect(third.doc).toHaveProperty('createdAt')
+    expect(third.doc).toHaveProperty('updatedAt')
+
+    expect(third.patch).toHaveLength(0)
+
+    // 4 update
+    expect(fourth.version).toBe(1)
+    expect(fourth.op).toBe('update')
+    expect(fourth.modelName).toBe('Product')
+    expect(fourth.collectionName).toBe('products')
+    expect(fourth.collectionId).toEqual(product._id)
+
+    expect(fourth.doc).toBeUndefined()
+
+    expect(fourth.patch).toHaveLength(2)
+    expect(fourth.patch).toMatchObject([
+      { op: 'test', path: '/addedBy', value: john._id.toString() },
+      { op: 'replace', path: '/addedBy', value: alice._id.toString() },
+    ])
+
+    expect(em.emit).toHaveBeenCalledTimes(4)
+    expect(em.emit).toHaveBeenCalledWith(GLOBAL_CREATED, { doc: first.doc })
+    expect(em.emit).toHaveBeenCalledWith(GLOBAL_CREATED, { doc: second.doc })
+    expect(em.emit).toHaveBeenCalledWith(GLOBAL_CREATED, { doc: third.doc })
+    expect(em.emit).toHaveBeenCalledWith(GLOBAL_UPDATED, {
+      oldDoc: expect.objectContaining({ _id: product._id, name: 'paper', addedBy: john._id }),
+      doc: expect.objectContaining({ _id: product._id, name: 'paper', addedBy: alice._id }),
+      patch: fourth.patch,
+    })
+  })
+
+  it('should update objectID', async () => {
+    const john = await User.create({ name: 'John', role: 'user' })
+    expect(john.name).toBe('John')
+    const alice = await User.create({ name: 'Alice', role: 'user' })
+    expect(alice.name).toBe('Alice')
+    const product = await Product.create({ name: 'paper', addedBy: john })
+    expect(product.name).toBe('paper')
+
+    await product.updateOne({
+      addedBy: alice,
+    }).exec()
+
+    await product.updateOne({
+      addedBy: { _id: john._id, name: 'John', role: 'manager' },
+    }).exec()
+
+    const history = await History.find({})
+    expect(history).toHaveLength(5)
+
+    const [first, second, third, fourth, fifth] = history
+
+    // 1 create
+    expect(first.version).toBe(0)
+    expect(first.op).toBe('create')
+    expect(first.modelName).toBe('User')
+    expect(first.collectionName).toBe('users')
+    expect(first.collectionId).toEqual(john._id)
+
+    expect(first.doc).toHaveProperty('_id', john._id)
+    expect(first.doc).toHaveProperty('name', 'John')
+    expect(first.doc).toHaveProperty('role', 'user')
+    expect(first.doc).toHaveProperty('createdAt')
+    expect(first.doc).toHaveProperty('updatedAt')
+
+    expect(first.patch).toHaveLength(0)
+
+    // 2 create
+
+    expect(second.version).toBe(0)
+    expect(second.op).toBe('create')
+    expect(second.modelName).toBe('User')
+    expect(second.collectionName).toBe('users')
+    expect(second.collectionId).toEqual(alice._id)
+
+    expect(second.doc).toHaveProperty('_id', alice._id)
+    expect(second.doc).toHaveProperty('name', 'Alice')
+    expect(second.doc).toHaveProperty('role', 'user')
+    expect(second.doc).toHaveProperty('createdAt')
+    expect(second.doc).toHaveProperty('updatedAt')
+
+    expect(second.patch).toHaveLength(0)
+
+    // 3 create
+    expect(third.version).toBe(0)
+    expect(third.op).toBe('create')
+    expect(third.modelName).toBe('Product')
+    expect(third.collectionName).toBe('products')
+    expect(third.collectionId).toEqual(product._id)
+
+    expect(third.doc).toHaveProperty('_id', product._id)
+    expect(third.doc).toHaveProperty('name', 'paper')
+    expect(third.doc).toHaveProperty('addedBy', john._id)
+    expect(third.doc).toHaveProperty('createdAt')
+    expect(third.doc).toHaveProperty('updatedAt')
+
+    expect(third.patch).toHaveLength(0)
+
+    // 4 update
+    expect(fourth.version).toBe(1)
+    expect(fourth.op).toBe('updateOne')
+    expect(fourth.modelName).toBe('Product')
+    expect(fourth.collectionName).toBe('products')
+    expect(fourth.collectionId).toEqual(product._id)
+
+    expect(fourth.doc).toBeUndefined()
+
+    expect(fourth.patch).toHaveLength(2)
+    expect(fourth.patch).toMatchObject([
+      { op: 'test', path: '/addedBy', value: john._id.toString() },
+      { op: 'replace', path: '/addedBy', value: alice._id.toString() },
+    ])
+
+    // 5 update
+    expect(fifth.version).toBe(2)
+    expect(fifth.op).toBe('updateOne')
+    expect(fifth.modelName).toBe('Product')
+    expect(fifth.collectionName).toBe('products')
+    expect(fifth.collectionId).toEqual(product._id)
+
+    expect(fifth.doc).toBeUndefined()
+
+    expect(fifth.patch).toHaveLength(2)
+    expect(fifth.patch).toMatchObject([
+      { op: 'test', path: '/addedBy', value: alice._id.toString() },
+      { op: 'replace', path: '/addedBy', value: john._id.toString() },
+    ])
+
+    expect(em.emit).toHaveBeenCalledTimes(5)
+    expect(em.emit).toHaveBeenCalledWith(GLOBAL_CREATED, { doc: first.doc })
+    expect(em.emit).toHaveBeenCalledWith(GLOBAL_CREATED, { doc: second.doc })
+    expect(em.emit).toHaveBeenCalledWith(GLOBAL_CREATED, { doc: third.doc })
+    expect(em.emit).toHaveBeenCalledWith(GLOBAL_UPDATED, {
+      oldDoc: expect.objectContaining({ _id: product._id, name: 'paper', addedBy: john._id }),
+      doc: expect.objectContaining({ _id: product._id, name: 'paper', addedBy: alice._id }),
+      patch: fourth.patch,
+    })
+    expect(em.emit).toHaveBeenCalledWith(GLOBAL_UPDATED, {
+      oldDoc: expect.objectContaining({ _id: product._id, name: 'paper', addedBy: alice._id }),
+      doc: expect.objectContaining({ _id: product._id, name: 'paper', addedBy: john._id }),
+      patch: fifth.patch,
+    })
+  })
 })
