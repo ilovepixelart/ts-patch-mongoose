1e7687767ba27cc619d8eb3dbd978a04656053bc
ilovepixelart <7249538+ilovepixelart@users.noreply.github.com>
2024-12-10 20:48:24 +0100

    Fix biome

--- src/helpers.ts
+++ src/helpers.ts
@@ -15,39 +15,39 @@ export const toObjectOptions: ToObjectOptions = {
 }
 
 export const useTTL = async <T>(opts: IPluginOptions<T>) => {
-  const name = 'createdAt_1_TTL'; // To avoid collision with user defined index / manually created index
+  const name = 'createdAt_1_TTL' // To avoid collision with user defined index / manually created index
   try {
-    const indexes = await History.collection.indexes();
-    const existingIndex = indexes?.find(index => index.name === name);
+    const indexes = await History.collection.indexes()
+    const existingIndex = indexes?.find((index) => index.name === name)
 
     // Drop the index if historyTTL is not set and index exists
     if (!opts.historyTTL && existingIndex) {
-      await History.collection.dropIndex(name);
-      return;
+      await History.collection.dropIndex(name)
+      return
     }
 
-    const milliseconds = ms(opts.historyTTL as string);
+    const milliseconds = ms(opts.historyTTL as string)
 
     // Drop the index if historyTTL is less than 1 second and index exists
     if (milliseconds < 1000 && existingIndex) {
-      await History.collection.dropIndex(name);
-      return;
+      await History.collection.dropIndex(name)
+      return
     }
 
-    const expireAfterSeconds = milliseconds / 1000;
+    const expireAfterSeconds = milliseconds / 1000
 
     if (existingIndex && existingIndex.expireAfterSeconds === expireAfterSeconds) {
       // Index already exists with the correct TTL, no need to recreate
-      return;
+      return
     }
 
     if (existingIndex) {
       // Drop the existing index if it exists and TTL is different
-      await History.collection.dropIndex(name);
+      await History.collection.dropIndex(name)
       // Create a new index with the correct TTL
-      await History.collection.createIndex({ createdAt: 1 }, { expireAfterSeconds, name });
+      await History.collection.createIndex({ createdAt: 1 }, { expireAfterSeconds, name })
     }
   } catch (err) {
-    console.error("Couldn't create or update index for history collection", err);
+    console.error("Couldn't create or update index for history collection", err)
   }
 }
