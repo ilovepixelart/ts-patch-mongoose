0d6f228afa623ef2d179f760b531c71d97d0a924
ilovepixelart <ilovepixelart@users.noreply.github.com>
2025-10-23 23:41:21 +0200

    Sonar

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import _ from 'lodash'
+import { isEmpty } from 'lodash-es'
 import em from './em'
 import { toObjectOptions } from './helpers'
 import { deleteHooksInitialize } from './hooks/delete-hooks'
@@ -65,7 +65,7 @@ export const patchHistoryPlugin = function plugin<T>(schema: Schema<T>, opts: Pl
       // @ts-expect-error - Mongoose 7 and below
       const original = this.toObject(toObjectOptions) as HydratedDocument<T>
 
-      if (opts.preDelete && !_.isEmpty(original)) {
+      if (opts.preDelete && !isEmpty(original)) {
         await opts.preDelete([original])
       }
     })
